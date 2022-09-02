208ab18486af63c34e4684b8e78fef34be9bf6c2
fracter <78301601+fracter2@users.noreply.github.com>
2022-09-02 09:14:29 +0200

    Made the runner run smoothly, and fast
    Also brings pre-emptive feature to push entities

--- src/Entities/Scripts/Runner.gd
+++ src/Entities/Scripts/Runner.gd
@@ -13,16 +13,19 @@ func _on_Area2D_body_exited(body):
 	player = null
 
 func _physics_process(delta):
+	
+	#velocity.x = 0
+	if player != null:
+		velocity.x += position.direction_to(player.position).x * run_speed * delta * 3
+		get_target()
+	velocity.y += gravity * delta
+	velocity *= 0.94
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	
+
+func get_target():
 	var overlapping_bodies: Array= $Area2D.get_overlapping_bodies()
 	for i in overlapping_bodies:
 		if i.is_in_group("Player"):
 			player = i
-	
-	velocity.x = 0
-	if player != null:
-		velocity.x = position.direction_to(player.position).x * run_speed
-	velocity.y += gravity * delta
-	
-	velocity = move_and_slide(velocity, Vector2.UP)
-	
-	
