4462b7c2a5ae1f1592a598f48a9e53d9a33d07b6
fracter <78301601+fracter2@users.noreply.github.com>
2022-09-07 09:34:20 +0200

    Named the 2D physics layers + misc comments

--- src/Player/scripts/Push Area.gd
+++ src/Player/scripts/Push Area.gd
@@ -2,8 +2,9 @@ extends Area2D
 
 export var knockback:int = 100
 
-# Called when the node enters the scene tree for the first time.
 
+
+# As it is now, it also gives knockback in the Y direction.
 func _physics_process(delta):
 	var entity_list:Array = get_entity_list()
 	if entity_list.size()!= 0:
@@ -13,12 +14,15 @@ func _physics_process(delta):
 			dir = dir.normalized()
 			i.velocity += dir * knockback
 			
-	
+
+# This part was to have an area where knockback got applied just once, like as a big shove. Redundant now.
 func _on_Push_Area_body_entered(body):
 	pass
 #	if body.is_in_group("Entity"):
 #		body.velocity.x += knockback
 		
 
+
 func get_entity_list() -> Array:
 	return get_overlapping_bodies()
+	# Add code to check if its actually pushable
