e5d232040f104d1b856161abc6ecd68aebc68469
fracter <78301601+fracter2@users.noreply.github.com>
2022-09-08 13:11:43 +0200

    Runner.gd cleanuup

--- src/Entities/Scripts/Runner.gd
+++ src/Entities/Scripts/Runner.gd
@@ -1,17 +1,18 @@
 extends KinematicBody2D
 
 
-
 export (int) var run_speed = 1000
 export (int) var gravity = 3500
 var velocity = Vector2.ZERO
 var player = null
+
 
 func _on_Area2D_body_entered(body):
 	player = body
 
 func _on_Area2D_body_exited(body):
 	player = null
+
 
 func _physics_process(delta):
 	
@@ -22,8 +23,7 @@ func _physics_process(delta):
 	velocity.y += gravity * delta
 	velocity *= 0.94
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
-	
+
 
 func get_target():
 	var overlapping_bodies: Array= $Area2D.get_overlapping_bodies()
