14b114358ff39fd924b0f4dcb6e3e3757a0f7b9a
fracter <78301601+fracter2@users.noreply.github.com>
2022-09-09 12:45:12 +0200

    Update Entity.gd
    hp, take_damage(), and die()

--- src/Entities/Scripts/Entity.gd
+++ src/Entities/Scripts/Entity.gd
@@ -2,7 +2,16 @@ extends KinematicBody2D
 
 class_name Entity
 
+var hp: int = 100
 
+func take_damage(var dmg:int):
+	hp -= dmg;
+	if hp < 0:
+		die()
+		
+
+func die():
+	queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
