7b7572a0bdf2502a7f323aa4199f27b0b4b605e9
fracter <78301601+fracter2@users.noreply.github.com>
2022-09-13 10:16:58 +0200

    Updated player controlls + experimented with wagons
    This will result in merge conflicts later on most likely

--- src/Player/scripts/Player.gd
+++ src/Player/scripts/Player.gd
@@ -28,6 +28,6 @@ func _physics_process(delta):
 	get_input()
 	velocity.y += gravity * delta
 	velocity = .move_and_slide(velocity, Vector2.UP)
-	if Input.is_action_just_pressed("move_jump"):
+	if Input.is_action_just_pressed("move_up"):
 		if .is_on_floor():
 			velocity.y = jump_speed
