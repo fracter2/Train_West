19274d0e7d83ba2521093766f9909dc61698b14b
fracter <78301601+fracter2@users.noreply.github.com>
2022-10-10 16:34:26 +0200

    hull integrity var

--- src/Singletons/train_manager.gd
+++ src/Singletons/train_manager.gd
@@ -3,6 +3,9 @@ extends Node
 # Put in everything relating to the train here
 # Examples: resources, hit points, speed, etc
 
+var Hull_integrity = 1000
+var Hull_integrity_max = 1000
+
 
 func _ready():
 	pass 
