7a09a56dadb6085dd37cc90f5354a08e90c4c821
Ludvig850 <ludvig850@gmail.com>
2022-10-13 15:03:29 +0200

    They are alive
    Two new AI, as well as some uppdates to movment

---
+++ src/Entities/Scripts/Enemy_Flying.gd
@@ -0,0 +1,60 @@
+extends Entity
+
+
+export (int) var run_speed = 1000
+#export (int) var gravity = 3500
+var velocity = Vector2.ZERO
+var player = null
+
+export(int) var attack_dmg := 20
+
+func _on_Area2D_body_entered(body):
+	player = body
+
+func _on_Area2D_body_exited(body):
+	player = null
+
+
+# Movement, and misc
+func _physics_process(delta):
+	#velocity.x = 0
+	if player != null:
+		var tops:Vector2 = position.direction_to(player.position)
+		if tops.x < 0: tops.x = -1
+		else: tops.x = 1
+		if tops.y < 0: tops.y = -1
+		else: tops.y = 1
+		velocity.x += tops.x * run_speed * delta * 3
+		velocity.y += tops.y * run_speed * delta * 3
+		get_target()
+	#velocity.y += gravity * delta
+	velocity *= 0.94
+	# velocity = move_and_slide(velocity, Vector2.UP)
+	
+	attackBoxUpdate()
+
+
+func _process(delta):
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+
+# Updates the "Player" position, for chasing it
+func get_target():
+	var overlapping_bodies: Array= $VisionBox.get_overlapping_bodies()
+	if overlapping_bodies.size() == 0:
+		return
+	for i in overlapping_bodies:
+		if i.is_in_group("Player"):
+			player = i
+			return
+
+# This is the current solution for damaging the opponent
+func attackBoxUpdate():
+	var targets: Array = $AttackBox.get_overlapping_bodies()
+	if targets.size() == 0: 
+		return # Skips the rest of this function
+	for i in targets:
+		i.take_damage(attack_dmg)
+	
+	
+	
