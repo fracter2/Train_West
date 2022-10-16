863ebe1a2683b3c242a7f36909c6aa0c653354fb
fracter <fracter.giftogod@gmail.com>
2022-10-16 04:13:03 +0200

    Wagons made killable and repairable

--- src/Train/wagon_blocks/scripts/Wagon_Entity.gd
+++ src/Train/wagon_blocks/scripts/Wagon_Entity.gd
@@ -5,6 +5,10 @@ class_name Wagon_Entity
 var hp:int = 100
 export var hp_max: int = 100
 
+enum STATES {ALIVE, DEAD}
+var state: int = 0
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hp = hp_max
@@ -13,6 +17,18 @@ func _ready():
 func take_damage(var dmg:int):
 	hp -= dmg
 	hp = clamp(hp, 0, hp_max)
+	
 	if hp == 0:
-		pass
+		state = STATES.DEAD
+		toggle_disabled(false)
+	else:
+		if state == STATES.DEAD:
+			toggle_disabled(true)
+		state = STATES.ALIVE
+	
+
+func toggle_disabled(var value:bool):
+	get_node("CollisionBase").set_deferred("Disabled", value)
+	get_node("PolygonBase").visible = not value
+	print("atempt disable")
 	
