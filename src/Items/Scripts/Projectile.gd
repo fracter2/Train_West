d91a11e0bb64807a865535260525222a92f6fece
fracter <78301601+fracter2@users.noreply.github.com>
2022-10-17 15:48:02 +0200

    Player weapon improovements

--- src/Items/Scripts/Projectile.gd
+++ src/Items/Scripts/Projectile.gd
@@ -16,3 +16,13 @@ func _ready():
 
 func _on_Despawn_Timer_timeout():
 	queue_free()
+
+
+func _on_Hitbox_body_entered(body):
+	body.take_damage(target_dmg)
+	queue_free()
+
+
+func _on_Hitbox_area_entered(area):
+	area.get_parent().take_damage(target_dmg)
+	queue_free()
--- src/Player/scripts/Weapon_1.gd
+++ src/Player/scripts/Weapon_1.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-export(Vector2) var force:Vector2 = Vector2(500, 0)
+export(Vector2) var force:Vector2 = Vector2(-5000, 0)
 export(Vector2) var knockback:Vector2 = Vector2(100, 0)
 export var rechamber_time:float = 0.2
 
@@ -25,8 +25,9 @@ func fire():
 	var proj = projectile.instance()
 	
 	proj.global_position = $"Firing Point".global_position
-	proj.rotation = position.angle_to_point(get_global_mouse_position())
-	proj.apply_central_impulse(force)
+	proj.rotation = global_position.angle_to_point(get_global_mouse_position())
+	print(global_position.angle_to_point(get_global_mouse_position()))
+	proj.apply_central_impulse(force.rotated(proj.rotation))
 	$"/root/World".add_child(proj)
 	#proj.look_at(get_global_mouse_position())
 	
