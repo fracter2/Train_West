9093087df5a3e7a3106e018a2704e3ab148ef5a9
fracter <78301601+fracter2@users.noreply.github.com>
2022-10-18 15:09:32 +0200

    Randomize singleton

--- src/Singletons/Events_manager.gd
+++ src/Singletons/Events_manager.gd
@@ -1,9 +1,12 @@
 extends Node
 
-
+onready var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
+func get_random_num(var from:float = 0, var to:float = 1) -> float:
+	return rng.randf_range(from, to)
+	
+	
