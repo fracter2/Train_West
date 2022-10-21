62def18f34e59aca6e9a979148553a63a5b9ec4d
fracter <78301601+fracter2@users.noreply.github.com>
2022-10-21 15:47:23 +0200

    repair tool

--- src/Player/scripts/Repair_Tool.gd
+++ src/Player/scripts/Repair_Tool.gd
@@ -11,9 +11,11 @@ func _ready():
 
 
 func _physics_process(delta):
-	if Input.is_action_pressed("action_2"):
-		# Particle Emission
-		pass
+	if Input.is_action_just_pressed("action_2"):
+		$Particles2D.emitting = true
+		
+	elif Input.is_action_just_released("action_2"):
+		$Particles2D.emitting = false
 	
 	if cooldown_frame == repair_cooldown_frames: 
 		if Input.is_action_pressed("action_2"):  
