b59b0435b060f6245ca9452e37acca5ec64fe418
fracter <fracter.giftogod@gmail.com>
2022-10-24 00:20:01 +0200

    Button development

---
+++ src/Train/Facilities/scripts/Button.gd
@@ -0,0 +1,8 @@
+extends Facility
+
+export var toggle:bool = false
+var pressed:bool = false
+
+signal button_just_pressed
+signal button_just_released
+
--- src/Train/Facilities/scripts/Facility.gd
+++ src/Train/Facilities/scripts/Facility.gd
@@ -4,13 +4,16 @@ extends Wagon_Entity
 # Chance to erode, every second roughly, maybe
 export(float, 0, 1) var erode_chance:float
 
+signal interacted(player)
+
+
 func toggle_disabled(var value:bool):
 	#get_node("CollisionBase").set_deferred("disabled", value)
 	get_node("PolygonBase").visible = not value
 
 
 func interact(var player):
-	pass
+	emit_signal("facility_interacted", player)
 	
 
 
