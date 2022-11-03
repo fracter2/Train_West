3a20b7dedb156f502ce83c3786e6ad0aaa8e1cf8
fracter <fracter.giftogod@gmail.com>
2022-11-04 00:25:42 +0100

    Smoke improvements

--- src/Train/wagon_blocks/scripts/Engine.gd
+++ src/Train/wagon_blocks/scripts/Engine.gd
@@ -6,7 +6,18 @@ export(float, 0, 10) var flash_recovery_rate:float = 4 # how much it recovers pe
 
 export var spawn_area:Vector2 = Vector2(512, 112)
 
+onready var smoke_emitter = $PolygonBase/Particles2D
+onready var smoke_default_material = $PolygonBase/Particles2D.process_material
+onready var smoke_default_gravity = $PolygonBase/Particles2D.process_material.gravity
 
+#func _ready():
+#	$PolygonBase/Particles2D.process_material.gravity.x
+
+
+func _physics_process(delta):
+	var new:Vector3 = smoke_default_gravity
+	new.x = Train_manager.velocity * -3
+	smoke_emitter.process_material.set_deferred("gravity", new)
 
 func update_effects(variable:int = 0):
 	var hp_fullness:float = (hp + color_offset) / (hp_max + color_offset) 
