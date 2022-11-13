85fa37bd5f9f35537d6b9106e9b2eff462d89b47
fracter <fracter.giftogod@gmail.com>
2022-11-13 11:36:47 +0100

    made a super advanced system to add corner shadows to wagon segments
    This took way too long

--- src/Train/wagon_blocks/scripts/Wagon_Segment_1.gd
+++ src/Train/wagon_blocks/scripts/Wagon_Segment_1.gd
@@ -1,9 +1,16 @@
 extends Wagon_Entity
 class_name Wagon_Segment
 
+export(bool) var left_corner_shadows:bool = false #setget set_left_corner
+export(bool) var right_corner_shadows:bool = false #setget set_right_corner
+
+
+
 var color_offset = 30
 export(float, 0, 1) var flash_ammount:float = 0.4 # (flash range: 0 - 1 )
 export(float, 0, 10) var flash_recovery_rate:float = 4 # how much it recovers per second
+
+
 
 
 func update_effects(variable:int = 0):
@@ -15,4 +22,32 @@ func update_effects(variable:int = 0):
 		$PolygonFlash.modulate.a = flash_ammount
 
 
+func on_ready_LC_check():
+	if left_corner_shadows:
+		set_left_corner(left_corner_shadows)
 
+func on_ready_RC_check():
+	if right_corner_shadows:
+		set_right_corner(right_corner_shadows)
+
+
+func set_left_corner(state:bool):
+	var poly = $PolygonBase/LightOccluderBaseLeft.occluder.polygon
+	if state and poly.size() <= 2:					# If it wants to enable it
+		poly.append(poly[1] * Vector2(1, -1))		# Reverse the Y coordinate
+	
+	elif not state and poly.size() >= 3:
+		poly.remove(2)								# Removes the last vertex
+	
+	$PolygonBase/LightOccluderBaseLeft.occluder.polygon = poly
+
+
+func set_right_corner(state:bool):
+	var poly = $PolygonBase/LightOccluderBaseRight.occluder.polygon
+	if state and poly.size() <= 2:					# If it wants to enable it
+		poly.append(poly[1] * Vector2(1, -1))		# Reverse the Y coordinate
+	
+	elif not state and poly.size() >= 3:
+		poly.remove(2)								# Removes the last vertex
+	
+	$PolygonBase/LightOccluderBaseRight.occluder.polygon = poly
