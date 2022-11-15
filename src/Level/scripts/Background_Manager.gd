3789b2c16a93cea10ca71499ca638e0fbac4be43
fracter <78301601+fracter2@users.noreply.github.com>
2022-11-15 14:20:51 +0100

    Background now moves... to the void.

--- src/Level/scripts/Background_Manager.gd
+++ src/Level/scripts/Background_Manager.gd
@@ -1,7 +1,19 @@
 extends Node2D
 
-var background1
-var background2 = 
+var back_vel_k:float = 40				# K is for koefficient
 
-func _ready():
-	pass
+var background1 = preload("res://src/Level/Enviornment/Background1.tscn")
+var background2 = preload("res://src/Level/Enviornment/Background2.tscn")
+
+var back1_width = 20200
+var back2_width = 20200
+
+var velocity:float = 0
+
+
+func _physics_process(delta):
+	velocity = Train_manager.velocity * back_vel_k
+	
+	$"Background-1/Background1".position.x += -velocity *delta	# Negative, since left is negative
+	$"Background-2/Background2".position.x += -velocity *delta	# and velocity is not a vector...
+															# shit, it should be called speed instead.
