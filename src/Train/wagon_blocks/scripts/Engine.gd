f67ef3d73c04e506b85078dcf6d8c519282fbf3f
fracter <78301601+fracter2@users.noreply.github.com>
2023-02-14 14:48:32 +0100

    Engine Indicators Function

--- src/Train/wagon_blocks/scripts/Engine.gd
+++ src/Train/wagon_blocks/scripts/Engine.gd
@@ -6,37 +6,42 @@ export(float, 0, 10) var flash_recovery_rate:float = 4 # how much it recovers pe
 
 export var spawn_area:Vector2 = Vector2(512, 112)
 
-onready var smoke_emitter = $PolygonBase/Particles2D
-onready var smoke_default_material = $PolygonBase/Particles2D.process_material
-onready var smoke_default_gravity = $PolygonBase/Particles2D.process_material.gravity
+onready var smoke_emitter = $PolygonBase/Smoke_Emitter
+onready var smoke_default_material = $PolygonBase/Smoke_Emitter.process_material
+onready var smoke_default_gravity = $PolygonBase/Smoke_Emitter.process_material.gravity
+export(float, -10,0) var smoke_velocity_ko:float = -3
+onready var fire_emitter = $Fire_Emitter
+export var fire_emit_min:float = 8
+export var fire_emit_max:int = 64
+export var fire_time_min:float = 0.5
+export var fire_time_max:float = 2.2
 onready var heat_radiance = $HeatRadiance
 export var heat_radiance_min:float = 0.3
 export var heat_radiance_max:float = 1
-onready var heat_max = Train_manager.engine_heat_max
-var sup:int = 0																# Simple update timer, for expensive operations
-var supm:int = 2
 #func _ready():
 #	$PolygonBase/Particles2D.process_material.gravity.x
 
 
-func _physics_process(delta):
+func _ready():
+	pass
+	$"/root/Train_manager".connect("heat_updated", self, "set_heat")
+	$"/root/Train_manager".connect("velocity_updated", self, "set_smoke")
+
+
+func set_heat(val:float):
+	heat_radiance.energy = lerp(heat_radiance_min, heat_radiance_max, val)
+	#fire_emitter.amount = lerp(fire_emit_min, fire_emit_max, val)
+	fire_emitter.lifetime = lerp(fire_time_min, fire_time_max, val)
+	
+	
+
+
+func set_smoke(val:float):
 	# Smoke particles effect
 	var new:Vector3 = smoke_default_gravity
-	new.x = Train_manager.velocity * -3
+	new.x = val * smoke_velocity_ko
 	smoke_emitter.process_material.set_deferred("gravity", new)
-	if sup >= supm:		# Simple frame_based timer, since this seems a little expensive
-		heat_radiance.energy = lerp(heat_radiance_min, heat_radiance_max, Train_manager.engine_heat / heat_max)
-		sup = 0
-	else:
-		sup += 1
-
-
-func update_effects(variable:int = 0):
-	var hp_fullness:float = (hp + color_offset) / (hp_max + color_offset) 
-	$PolygonBase.modulate = Color(hp_fullness, hp_fullness, hp_fullness)
 	
-	if variable == 0: # If it was an attack
-		$PolygonBase/PolygonFlash.modulate.a = flash_ammount
 
 
 
